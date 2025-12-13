b2caea31b3c37147b68658603d7cf81833424b38
kads2222 <sithulikaluarachchi@gmail.com>
2025-12-13 10:41:15 +0530

    Add mini session, long session, custom session and break

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ let timeDisplay = document.getElementById("time");
 let startBtn = document.getElementById("start-pause");
 let startIcon = document.getElementById("start-pause-icon");
 let resetBtn = document.getElementById("reset");
+let miniSession = document.getElementById("mini");
+let longSession = document.getElementById("long");
+let breakSession = document.getElementById("break");
+let openModalbtn = document.getElementById("custom");
+let modal = document.getElementById("modal");
+let timeInput = document.getElementById("timeInput");
+let error = document.getElementById("errorMsg");
+let timeSetBtn = document.getElementById("setCustom");
+let cancelBtn = document.getElementById("cancel");
 
 let totalTime = 25 * 60;
 let timer = null;
@@ -29,7 +38,8 @@ function startTimer() {
         isRunning = false;
         startIcon.classList.remove("fa-pause");
         startIcon.classList.add("fa-play");
-        alert("time is up");
+        totalTime = 25 * 60;
+        updateDisplay();
       }
     }, 1000);
   } else {
@@ -49,7 +59,45 @@ function resetTimer() {
   updateDisplay();
 }
 
+function displayTime(time) {
+  totalTime = time * 60;
+  updateDisplay();
+}
+
+function customTime(duration) {
+  if (duration <= 60 && duration > 0) {
+    errorMsg.textContent = "";
+    displayTime(duration);
+    modal.style.display = "none";
+  } else {
+    error.textContent = "Please enter the session between 1 and 60 minutes";
+  }
+}
+
 startBtn.addEventListener("click", startTimer);
 resetBtn.addEventListener("click", resetTimer);
 
+miniSession.addEventListener("click", () => {
+  displayTime(25);
+});
+longSession.addEventListener("click", () => {
+  displayTime(60);
+});
+breakSession.addEventListener("click", () => {
+  displayTime(5);
+});
+
+openModalbtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+cancelBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+timeSetBtn.addEventListener("click", () => {
+  customTime(timeInput.value);
+});
+
 updateDisplay();
+modal.style.display = "none";
